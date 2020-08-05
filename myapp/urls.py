c04fa287b337149e6f7c64b0c0cde6a66c8a7688
chandra mouli <pchandramouli92@gmail.com>
2020-08-05 20:36:00 +0530

    first commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,6 +6,8 @@ app_name = "myapp"
 urlpatterns = [
    path('trail/',views.trail,name='trail'),
     path('profile/',views.profile,name='profile'),
+     path('',views.home,name="home"),
+     path('base/',views.base,name="base"),
     
 ]
 
